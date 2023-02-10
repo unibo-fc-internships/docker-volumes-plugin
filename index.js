42d87650b066c36c2243954b88d71253cf864a36
Martina <martina.baiardi4@studio.unibo.it>
2023-02-10 16:39:53 +0100

    feat: wip on plugin implementation

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const http = require('http');
-const fs =  require('fs');
+const fs =  require('fs-extra');
+const execSync = require('child_process').execSync;
 const net = require('net');
 const app = express();
 const server = http.createServer(app);
 
+// Body parser
+app.use(express.json())
+
+// var directories = new Map([
+//     ['/mnt/data2/', 0],
+//     ['/mnt/data3/', 0],
+//     ['/mnt/data4/', 0],
+//     ['/mnt/data5/', 0],
+//     ['/mnt/data6/', 0],
+//     ['/mnt/data7/', 0],
+//     ['/mnt/data8/', 0],
+//     ['/mnt/data9/', 0],
+// ]);
+
+var directories = new Map([
+    ['/mnt/data2/', 0],
+    ['/mnt/data3/', 0],
+])
+
+var volumes = new Map();
 
 app.post('/Plugin.Activate', (req, res) => {
-  console.log("Request to Activate")
   res.json({"Implements": ["VolumeDriver"]})
 })
 
 app.post('/VolumeDriver.Create', (req, res) => {
-  res.send('Create')
+    if (req.body === undefined){
+        res.json({"Err": "Request is not correctly formatted"})
+    } else if (volumes.has(req.body.Name)) {
+        res.json({"Err": "Volume " + name + " already exists"})
+    } else {
+        const name = req.body.Name;
+        // Calculate directories occupation
+        directories.forEach((_ , folder) => {
+            console.log(execSync('du -s ' + folder))
+            const size = parseInt(execSync('du -s ' + folder + ' | cut -f1', { encoding: 'utf-8' }))
+            directories.set(folder, size)
+        })
+        console.log(directories)
+        const min = Math.min(...directories.values())
+        const s = [...directories.entries()].find(([_, v]) => v === min)[0]
+        console.log(s + name)
+        fs.mkdir(s + name, (err) => {
+            if (err) {
+              res.json({"Err": "Volume " + name + " cannot be created. " + err})
+            } else {
+              res.send('OK')
+            }
+        })
+    }
 })
 
 app.post('/VolumeDriver.Get', (req, res) => {
