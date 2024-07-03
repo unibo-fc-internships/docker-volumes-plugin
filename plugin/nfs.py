d37f8239486c412d04908fd692b16e75137692e1
francois <francois.jourden@studio.unibo.com>
2024-07-03 12:26:02 +0200

    feat: mount path in plugin now decided by plugin

--- plugin/nfs.py
+++ plugin/nfs.py
@@ -3,7 +3,7 @@ import os
 from plugin._log import logging
 from dataclasses import dataclass
 import typing
-
+import subprocess
 
 ROOT = pathlib.Path("/var/lib/docker/x-drives")
 
@@ -17,17 +17,29 @@ class NFSFilesystem:
 
     def __post_init__(self):
         self.options = self.options or set()
- 
+
     @classmethod
     def parse(cls, string: str) -> 'NFSFilesystem':
-        """parses the <IP>:<remote_address> <local_address> <options> format"""
+        """
+        init an NFSFilesystem object from a string
+        :param str string: '\<server>:\<remote_path> \<options>(option)' format
+        """
+
+        def sanitize_string(s: str):
+            forbidden_chars = '/'
+            return ''.join('_' if c in forbidden_chars else c for c in s)
+
         parts = string.split()
-        assert len(parts) in {2,3}, f"Invalid NFS mount point string: {string}"
-        options = set() if len(parts) == 2 else { s.strip() for s in parts[2].split(",") }
+        assert len(parts) in {1, 2}, f"Invalid NFS mount point string: {string}"
+
+        options = set() if len(parts) == 1 else {s.strip() for s in parts[2].split(",")}
+
         server, remote_path = parts[0].split(":")
         server = server.strip()
         remote_path = pathlib.Path(remote_path.strip())
-        local_path = pathlib.Path(parts[1].strip())
+
+        local_path = pathlib.Path(ROOT / str(sanitize_string(parts[0])))
+
         return cls(server, remote_path, local_path, options)
 
     def __str__(self):
@@ -36,7 +48,10 @@ class NFSFilesystem:
     def mount(self):
         if not self.local_path.exists():
             self.local_path.mkdir(parents=True)
-        assert os.system(str(self)) == 0, f"Failed to mount {self.server}:{self.remote_path} to {self.local_path}"
+
+        # assert os.system(str(self)) == 0, f"Failed to mount {self.server}:{self.remote_path} to {self.local_path}"
+        process = subprocess.run(str(self).split())
+        process.check_returncode()
 
 
 NFS_MOUNTS = [
@@ -44,13 +59,6 @@ NFS_MOUNTS = [
     for k in os.environ.keys()
     if k.startswith("NFS_MOUNT")
 ]
-
-for mount in NFS_MOUNTS:
-    if mount.local_path.is_absolute:
-        raise ValueError("Only relative local paths are allowed. This is not a valid local path: " + mount.local_path)
-    else: 
-        mount.local_path = ROOT / mount.local_path
-
 
 if __name__ == "__main__":
     logging.info(f"NFS directories to mount: {len(NFS_MOUNTS)}")
