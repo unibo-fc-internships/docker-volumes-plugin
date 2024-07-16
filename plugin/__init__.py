2b37f7fa9ff17a31cb40790cea64ea9d9177b9cb
francois <francois.jourden@studio.unibo.com>
2024-07-16 16:19:06 +0200

    fix: error with env data

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,1 +1,11 @@
+import os
+from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv(Path(__file__).parent.parent / '.env')
+
+if not os.environ.get('PLUGIN_NAME'):
+    raise ValueError('PLUGIN_NAME is not set in .env file')
+if not os.environ.get('PLUGIN_NAME_SHORT'):
+    raise ValueError('PLUGIN_NAME_SHORT is not set in .env file')
