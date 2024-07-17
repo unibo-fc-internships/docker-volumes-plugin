9c2426f923c92ebdf7e98004d8a903eb0a5fd502
francois <francois.jourden@studio.unibo.com>
2024-07-17 16:43:04 +0200

    feat!: deploy with semantic-release

---
+++ release.config.js
@@ -0,0 +1,37 @@
+var dryRun = (process.env.RELEASE_DRY_RUN || "false").toLowerCase() === "true";
+var dockerhub_username = process.env.DOCKER_HUB_USERNAME;
+var dockerhub_password = process.env.DOCKER_HUB_PASSWORD;
+
+var prepareCmd = `docker login -u ${dockerhub_username} -p ${dockerhub_password}`;
+var publishCmd = `docker publish PLUGIN_TAG=${nextRelease.version}`
+
+if (dryRun) {
+    publishCmd = publishCmd.replace(" publish ", " build ");
+}
+
+var config = require('semantic-release-preconfigured-conventional-commits');
+
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "prepareCmd" : prepareCmd,
+        "publishCmd": publishCmd,
+    }]
+)
+
+if (!dryRun) {
+    config.plugins.push(
+        ["@semantic-release/github", {
+            "assets": [
+                { "path": "dist/*" },
+            ]
+        }],
+        ["@semantic-release/git", {
+            "assets": [
+                "CHANGELOG.md",
+            ],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }]
+    );
+}
+
+module.exports = config
