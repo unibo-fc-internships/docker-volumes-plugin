d9a6bd4c61a5518cb7021d517b27294f2a0e826a
francois <francois.jourden@studio.unibo.com>
2024-07-18 11:53:40 +0200

    fix: docker login failed

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ var dryRun = (process.env.RELEASE_DRY_RUN || "false").toLowerCase() === "true";
 var dockerhub_username = process.env.DOCKER_HUB_USERNAME;
 var dockerhub_password = process.env.DOCKER_HUB_PASSWORD;
 
-var prepareCmd = `echo "${dockerhub_password}" | docker login -u "${dockerhub_username}" --password-stdin`;
+var prepareCmd = `echo "${dockerhub_password}" | docker login --username "${dockerhub_username}" --password-stdin`;
 var publishCmd = "docker publish PLUGIN_TAG=\${nextRelease.version}"
 
 if (dryRun) {
