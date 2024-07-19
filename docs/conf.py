360f4181b960d4de35f59d588d0203c73ec3cd97
francois <francois.jourden@studio.unibo.com>
2024-07-19 12:51:24 +0200

    conf doc

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "NFS-volumes docker plugin"
+copyright = "2024"
+author = "François Jourden"
 
 
 # -- General configuration ---------------------------------------------------
